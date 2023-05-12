ab1c4cbbe5bde4e0d50410421fef8d0f034355a5
Arshia Balbadorre <arshia.balbadorre@gmail.com>
2023-05-12 10:27:32 +0430

    list of movies & series with cover & thumb created

---
+++ src/assets/movie/list.js
@@ -0,0 +1,156 @@
+import serieOne from "../movie-pictures/series/01.webp";
+import serieCoverOne from "../movie-pictures/series/cover01.webp";
+import serieTwo from "../movie-pictures/series/02.webp";
+import serieCoverTwo from "../movie-pictures/series/cover02.webp";
+import serieThree from "../movie-pictures/series/03.webp";
+import serieCoverThree from "../movie-pictures/series/cover03.webp";
+import serieFour from "../movie-pictures/series/04.webp";
+import serieCoverFour from "../movie-pictures/series/cover04.webp";
+import serieFive from "../movie-pictures/series/05.webp";
+import serieCoverFive from "../movie-pictures/series/cover05.webp";
+import serieSix from "../movie-pictures/series/06.webp";
+import serieCoverSix from "../movie-pictures/series/cover06.webp";
+import movieOne from "../movie-pictures/movies/01.webp"
+import movieCoverOne from "../movie-pictures/movies/cover01.webp"
+import movieTwo from "../movie-pictures/movies/02.webp"
+import movieCoverTwo from "../movie-pictures/movies/cover02.webp"
+import movieThree from "../movie-pictures/movies/03.webp"
+import movieCoverThree from "../movie-pictures/movies/cover03.webp"
+import movieFour from "../movie-pictures/movies/04.webp"
+import movieCoverFour from "../movie-pictures/movies/cover04.webp"
+import movieFive from "../movie-pictures/movies/05.webp"
+import movieCoverFive from "../movie-pictures/movies/cover05.webp"
+import movieSix from "../movie-pictures/movies/06.webp"
+import movieCoverSix from "../movie-pictures/movies/cover06.webp"
+
+export const movieList = [[{
+    id: 0,
+    thumb: serieOne,
+    cover: serieCoverOne,
+    title: "مهمونی",
+    episode: "Mehmooni S02E09",
+    director: "ایرج طهماسب",
+    rate:92,
+    description:"سلام! خوبین؟ خوشین؟ سلامتین؟ با مجموعه مهمونی در خدمت شما هستیم.",
+    tags:["کمدی","خانوادگی"]
+},
+{
+    id: 1,
+    thumb: serieTwo,
+    cover: serieCoverTwo,
+    title: "نیوکمپ",
+    episode: "Camp Nou S01E01",
+    director: "منوچهر هادی",
+    rate:93,
+    description:"«نیوکمپ» قصه خانواده سرچشمه را در جستجوی رویاهای کوچک‌شان روایت می‌کند که رویایی بزرگ در خانه آن‌ها را می‌زند.",
+    tags:["کمدی","خانوادگی"]
+},
+{
+    id: 2,
+    thumb: serieThree,
+    cover: serieCoverThree,
+    title: "دیو و ماه پیشونی",
+    episode: "The Demon and the Forhead Moon S01E10",
+    director: "حسین قناعت",
+    rate:96,
+    description:"ماه پیشونی و ارژنگ با کمک مردم شهر فسفودی خان را شکست می دهند، پیروز می شوند و به شهری تازه می روند. در آن جا با مانعی بزرگتر روبرو م",
+    tags:["درام","خانوادگی"]
+},
+{
+    id: 3,
+    thumb: serieFour,
+    cover: serieCoverFour,
+    title: "تی ان تی",
+    episode: "TNT S01E06",
+    director: "سجاد افشاریان",
+    rate:77,
+    description:"هرکسی یا روز می میرد یا شب من شبانه روز قصه این روزهای تمام ماست ، تمام آن ها که در این خانه مانده اند و تمام آن ها که از این خان",
+    tags:["درام","خانوادگی"]
+},
+{
+    id: 4,
+    thumb: serieFive,
+    cover: serieCoverFive,
+    title: "تورقوزآباد",
+    episode: "Turquz Abad",
+    director: "علی شاه حاتمی",
+    rate:0,
+    description:"دروغ هیچ‌وقت کارساز نیست! عمدتا دردسرساز می‌شود...",
+    tags:["کمدی","درام"]
+},
+{
+    id: 5,
+    thumb: serieSix,
+    cover: serieCoverSix,
+    title:"چند میگیری گریه کنی 2",
+    episode: "How Much Will You Take to Cry 2",
+    director: "علی توکل نیا",
+    rate:75,
+    description:"داستان «چند میگیری گریه کنی 2» از جایی آغاز می شود که پسر پیرمرد ثروتمند قسمت اول این فیلم بعد از سی سال به ایران بر می گردد تا ارث پد...",
+    tags:["عاشقانه","خانوادگی"]
+},],[{
+    id: 0,
+    thumb: movieOne,
+    cover: movieCoverOne,
+    title: "بام بالا",
+    episode: "Bam Bala",
+    director: "سیدجمال سیدحاتمی",
+    rate:89,
+    description:"فیلم «بام بالا»، داستان خواهر و برادری به نام حنا و امیرعلی است که از عروسکی به نام بام بالا کمک می گیرند تا پدر و مادر خود را آشتی دهند. بام",
+    tags:["ماجراجویی","خانوادگی"]
+},
+{
+    id: 1,
+    thumb: movieTwo,
+    cover: movieCoverTwo,
+    title: "هناس",
+    episode: "Henas",
+    director: "حسین دارابی",
+    rate:93,
+    description:"فیلم «هناس»، روایتی متفاوت از زندگی شهید هسته ای، داریوش رضایی نژاد، است. شهره همسر او به خاطر تهدیدهایی که متوجه خانواده اش است",
+    tags:["بیوگرافی","درام"]
+},
+{
+    id: 2,
+    thumb: movieThree,
+    cover: movieCoverThree,
+    title: "تی تی",
+    episode: "Titi",
+    director: "آیدا پناهنده",
+    rate:89,
+    description:"یک فیزیکدان به شدت بیمار که بر روی نظریه ای در مورد سیاهچاله ها و پایان جهان کار می کند، در بیمارستان با یک خانم نظافتچی نسبتاً عجی...",
+    tags:["درام","عاضقانه"]
+},
+{
+    id: 3,
+    thumb: movieFour,
+    cover: movieCoverFour,
+    title: "هر کسی یا روز می‌ میرد یا شب من شبانه روز",
+    episode: "Death knocks at night or during the day, and I die around the clock",
+    director: "حامد آهنگی",
+    rate:91,
+    description:"قراره آدرنالین‌مون بچسبه به سقف...",
+    tags:["ریلیتی شو","گیم شو"]
+},
+{
+    id: 4,
+    thumb: movieFive,
+    cover: movieCoverFive,
+    title: "رهایم کن",
+    episode: "Set Me Free S01E12",
+    director: "شهرام شاه حسینی",
+    rate:97,
+    description:"حاتم به کمک ناصر جمعی را تدارک می ببیند تا در حضور همگان راز دل خود را فاش کرده و ماجرای مارال و هاتف را برای همیشه تمام ...",
+    tags:["عاشقانه","خانوادگی"]
+},
+{
+    id: 5,
+    thumb: movieSix,
+    cover: movieCoverSix,
+    title: "آکتور",
+    episode: "Actor S01E15",
+    director: "نیما جاویدی",
+    rate:96,
+    description:"در خانه صمديان، سارا متوجه می شود كسی كه صمديان به عنوان مادرش معرفی كرده كارگر خانه اش بوده است. از طرفی مرتضی كه بعد از ت...",
+    tags:["کمدی","خانوادگی"]
+}]];
