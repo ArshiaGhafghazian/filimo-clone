5d3fa497e04e8a13f189688b723cb015846ebde3
Arshia Balbadorre <arshia.balbadorre@gmail.com>
2023-05-16 16:14:28 +0430

    Watch Component completed!

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import Slider from '../Slider/Slider';
-import Best from '../Best/Best';
-import Devices from '../Devices/Devices';
+import React from "react";
+import Slider from "../Slider/Slider";
+import Best from "../Best/Best";
+import Devices from "../Devices/Devices";
+import Watch from "../Watch/Watch";
 
 const Home = () => {
     return (
         <>
-        <Slider />
-        <Best />
-        <Devices />
+            <Slider />
+            <Best />
+            <Devices />
+            <Watch />
         </>
     );
-}
+};
 
 export default Home;
--- src/Components/Watch/Watch.jsx
+++ src/Components/Watch/Watch.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
+
+import TVBG from "../../assets/pics/TVBG.png";
+import TVSM from "../../assets/pics/TVSM.webp";
+import androidTV from "../../assets/icons/androidTV.webp";
+import laptop from "../../assets/icons/laptop.webp";
+
+//style
+
+import "./Watch.css";
 
 const Watch = () => {
     return (
-        <div className='watch__container'>
-            
+        <div className="watch__container">
+            <div className="watch__image__container">
+                <img className="tvbg" src={TVBG} alt="tv-big" />
+            </div>
+            <div className="watch__title__container">
+                <h2>چطور با تلویزیون، فیلیمو تماشا کنم؟</h2>
+                <p>
+                    ما برای راحتی شما و تماشای فیلیمو روی تلویزیون‌های مختلف 6
+                    روش را فراهم کردیم. هرکدام از این 6 روش یک ویدیو آموزشی
+                    دارند که می‌توانید از لینک زیر، آن‌ها را تماشا کنید و روشی
+                    که با تلویزیون شما سازگار است را انتخاب کنید.
+                </p>
+                <img className="tvsm" src={TVSM} alt="tv-small" />
+                <div className="tv__container">
+                    <img src={laptop} alt="tv" />
+                    <div className="tv">
+                        <h2>تلویزیون</h2>
+                        <p>
+                            Amazon Fire TV - LG TVs - Chrome Cast - Apple TV -
+                            Android TV devices - Samsung
+                        </p>
+                    </div>
+                </div>
+                <div className="tv__container">
+                    <img src={androidTV} alt="android-tv" />
+                    <div className="tv">
+                        <h2>اندروید تی‌وی</h2>
+                        <p>
+                            NVIDIA - amazon - xiaomi - minix - skystream -
+                            turewell - ematic - humax - matricom
+                        </p>
+                    </div>
+                </div>
+                <button>ویدیوهای آموزشی تماشا با تلویزیون </button>
+            </div>
         </div>
     );
-}
+};
 
 export default Watch;
